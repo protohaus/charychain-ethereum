699b1374aedf23c64176c0fad2d657733da4e98b
AntjeNett <62296763+AntjeNett@users.noreply.github.com>
2022-10-29 16:43:58 +0200

    Update Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,7 +2,7 @@
 pragma solidity = 0.8.13; 
 
 import "./ILottery.sol"; 
-
+//all lotteryfunctions to execute donations-service
 contract Lottery is ILottery{
     address public managerpool; 
     address[] public donors;
@@ -19,13 +19,6 @@ contract Lottery is ILottery{
         donationsClosed = false;  
     }
 
-    //implement later 
-    /*function enterLottery(address[] memory _donators, bytes32 _sealedSeed) external returns (bool){
-        donors = _donators; 
-        setSealedSeed(_sealedSeed); 
-        return true; 
-    }*/
-
     function enterLottery(address[] memory _donators) external returns (bool){
         donors = _donators; 
         return true; 
@@ -36,30 +29,6 @@ contract Lottery is ILottery{
         numberwinner = random(); 
         address winner = donors[numberwinner]; 
         return winner; 
-    }
-
-    function setSealedSeed(bytes32 _sealedSeed) public {
-        require (!seedSet, "seed is already set");
-        require (msg.sender == managerpool, "only managerpool can call this function");
-        donationsClosed = true; 
-        sealedSeed = _sealedSeed; 
-        storedBlockNumber = block.number + 1; 
-        seedSet = true; 
-    }
-
-    /*function reveal(bytes32 _seed) public returns (uint){
-        require(seedSet, "seed is not yet set"); 
-        require(donationsClosed, "donations are not closed yet"); 
-        require(storedBlockNumber < block.number); 
-        require(keccak256(abi.encode(msg.sender, _seed))== sealedSeed); 
-        uint random = uint(keccak256(abi.encode(_seed, blockhash(storedBlockNumber))));
-        //require(keccak256(msg.sender, _seed) == sealedSeed); 
-        return random; 
-    }*/
-    //works with 66bytes 
-    function test(bytes32 _test) public returns(bool) {
-        //sealedSeed = abi.encodePacked(_test); 
-        return true; 
     }
 
     function getSize() public returns(uint){
